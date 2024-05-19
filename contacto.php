8ef148866478060b6886c74ff975c709d23c305f
Miguel González <miguel.gonzalez.93@gmail.com>
2024-05-19 04:22:03 -0400

    Add missing pages

---
+++ contacto.php
@@ -0,0 +1,80 @@
+<?php
+require_once(dirname(__FILE__) . '/header.php');
+?>
+<div class="bordes">
+	<table width="100%" height="365" border="0" cellspacing="2" cellpadding="2">
+		<tbody>
+			<tr>
+				<td width="5" align="center"></td>
+				<td height="35%" align="left" valign="top">
+					<div align="center" class="size15">
+						<b>Contacto</b><br /><br />
+					</div>
+					<div class="size11" style="margin-left: 30px;">
+						Ante cualquier problema, recomendaci&oacute;n, sugerencia, cr&iacute;tica, etc. puedes recurrir a cualquiera de nuestros miembros del staff.<br />
+						Se dispondr&aacute; de una casilla de correo a la cual podr&aacute;s dirigirte. Si eres un usuario registrado, puedes utilizar los mensajes del sitio.<br />
+						Tambi&eacute;n podrás dirigirte al correo de contacto de <?php echo $name; ?>: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>.
+						<br /><br />
+						<table style="font-size: 12px;">
+							<tbody>
+								<tr>
+									<td colspan="3" style="font-weight: bold;">
+										Administradores
+									</td>
+								</tr>
+								<tr>
+									<td width="200">
+										gonza1988
+									</td>
+									<td>
+										<a href="mailto:gonza1988@apuntatelo.com.ar">gonza1988@apuntatelo.com.ar</a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										cha
+									</td>
+									<td>
+										<a href="mailto:cha@apuntatelo.com.ar">cha@apuntatelo.com.ar</a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										nisiquieraunaparte
+									</td>
+									<td>
+										<a href="mailto:nisiquieraunaparte@apuntatelo.com.ar">nisiquieraunaparte@apuntatelo.com.ar</a>
+									</td>
+								</tr>
+								<tr>
+									<td colspan="23" style="padding-top: 20px; font-weight: bold;">
+										Moderadores
+									</td>
+								</tr>
+								<tr>
+									<td>
+										fidoqac
+									</td>
+									<td>
+										<a href="mailto:fidoqac@apuntatelo.com.ar">fidoqac@apuntatelo.com.ar</a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										nikPalu
+									</td>
+									<td>
+										<a href="mailto:nikpalu@apuntatelo.com.ar">nikpalu@apuntatelo.com.ar</a>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<?php
+require_once(dirname(__FILE__) . '/footer.php');
+?>
---
+++ protocolo.php
@@ -0,0 +1,70 @@
+<?php
+require_once(dirname(__FILE__) . '/header.php');
+?>
+<div class="bordes">
+  <table width="100%" height="365" border="0" cellspacing="2" cellpadding="2">
+    <tbody>
+      <tr>
+        <td width="5" align="center"></td>
+        <td height="35%" align="left" valign="top">
+          <div align="center" class="size15">
+            <b>Protocolo</b><br /><br />
+          </div>
+          <div class="size11">
+            <?php echo $name; ?> es una p&aacute;gina dise&ntilde;ada para compartir apuntes y diversas herramientas para aquellos que se encuentran cursando una carrera universitaria.<br />
+            <?php echo $name; ?> no se hace responsable por el contenido de los posts y/u opiniones de los usuarios en comentarios y publicaciones.<br /><br />
+            <b>En general:</b><br /><br />
+            Se prohibe totalmente la publicaci&oacute;n de pornograf&iacute;a y desnudez. Cualquier enlace de esta categor&iacute;a publicado por un usuario<br />
+            llevar&aacute; a la suspensi&oacute;n de por vida del mismo.<br /><br /> 
+            <b>De los post:</b><br /><br /> 
+            Se eliminan los post que contengan:<br />
+            <ul>
+              <li>Otro tipo de contenido que no sea apuntes o herramientas universitarias.</li>
+              <li>Informaci&oacute;n personal propia o de terceros tales como correos, MSN, nombres, tel&eacute;fonos, etc.</li>
+              <li>Material que ya fue posteado anteriormente. Excepto descargas que pueden dejar de funcionar con el tiempo.</li>
+              <li>Noticias que no contienen fuente. (Es necesario agregar el enlace correspondiente).</li>
+              <li>T&iacute;tulos poco descriptivos, que sean CON MAY&Uacute;SCULA (parcialmente o totalmente).</li>
+              <li>Contenido de mala calidad o mal hecho.</li>
+              <li>Enlaces a p&aacute;ginas/blogs personales/propias/amigos.</li>
+              <li>Mensajes a otros usuarios o a los moderadores.</li>
+              <li>Mensajes o contenido con el claro objetivo de buscar pol&eacute;mica. (pol&iacute;tica, f&uacute;tbol, etc).</li>
+              <li>Enlaces de torrents, pando, emule, etc. (P2P, P2M).</li>
+              <li>Contrase&ntilde;as o accesos privados (Rapidshare, Megaupload, Cuentas de correo, etc).</li>
+            </ul>
+            Se cierran los posts que contienen:<br />
+            <ul>
+              <li>Temas demasiados pol&iacute;ticos que pueden llegar a ofender a otros usuarios, ya sea por su contenido o comentarios.</li>
+              <li>Post que se transforman en un foro o se genera pol&eacute;mica. (famoso forobardo).</li>
+            </ul>
+            <b>De los comentarios:</b><br /><br />
+            Se eliminan comentarios que contengan:<br />
+            <ul>
+              <li>May&uacute;sculas o con abuso de may&uacute;sculas.</li>
+              <li>Insultos, ofensas, etc (a otro usuario o de forma general).</li>
+              <li>Comentarios racistas y/o peyorativos.</li>
+              <li>Letras muy grandes o con el claro efecto de llamar la atenci&oacute;n.</li>
+            </ul>
+            <b>De los usuarios:</b><br /><br />
+            Se eliminan o suspende a los usuarios que:<br />
+            <ul>
+              <li>Hacen post o comentarios racistas (Cualquier insulto racista, as&iacute; sea de uso "com&uacute;n" o hace referencia a alg&uacute;n pa&iacute;s lim&iacute;trofe).</li>
+              <li>Buscan generar pol&eacute;mica por medio de comentarios desafiantes o descalificantes.</li>
+              <li>Insultan a otro usuario, moderador y/o administrador.</li>
+              <li>Usuarios que agregan enlaces con sistemas de referencia del tipo "marketing piramidal" con el fin de obtener beneficios personales (Referer).</li>
+              <li>Usuarios a los cuales se les identifica m&aacute;s de una cuenta personal sin importar el fin de las mismas.</li>
+            </ul>
+            <b>Puntuar posts:</b><br /><br />
+            <ul>
+              <li>Cada 24 horas recibir&aacute;s una cantidad de puntos para repartir en los posts que m&aacute;s te interesen, no se podr&aacute; puntuar m&aacute;s de una vez cada post.<br /> 
+              &nbsp;&nbsp;La cantidad de puntos para otorgar depende de tu rango en la web.</li>
+            </ul>	
+            <br /><br />
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<?php
+require_once(dirname(__FILE__) . '/footer.php');
+?>
